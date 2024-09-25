d1ee8f2aa4042551ad38ddf8108d78718c465306
sarahwonago <sarahshaaleh@gmail.com>
2024-09-25 10:27:07 +0300

    island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Island Perimeter.
+"""
+
+
+def island_perimeter(grid):
+    """
+    Returns the perimeter of the island described in grid
+    
+    """
+    
+    area = 0
+    for row in grid + list(map(list, zip(*grid))):
+        for i1, i2 in zip([0] + row, row + [0]):
+            area += int(i1 != i2)
+    return area
